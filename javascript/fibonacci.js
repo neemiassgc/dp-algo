e9d81352110e30cb22f3cf90f9a0f708cedc68ff
Neemias Santos <neemiassgc@outlook.com>
2021-12-01 07:00:35 -0300

    Optimize to memoized solution of the Fibonacci problem in Javascript

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,6 +1,9 @@
-function fib(n) {
+function fib(n, memo = {}) {
+    if (memo[n]) return memo[n]
     if (n <= 2) return 1;
-    return fib(n - 1) + fib(n - 2)
+
+    memo[n] = fib(n - 1, memo) + fib(n - 2, memo)
+    return memo[n]
 }
 
 console.log(fib(50))
