7bbb123595486a42c9f4bb8750ed95f736aee7c4
Neemias Santos <neemiassgc@outlook.com>
2021-12-01 07:10:23 -0300

    Optimize to memoized solution of the GridTraveler problem in Javascript

--- javascript/gridTraveler.js
+++ javascript/gridTraveler.js
@@ -1,8 +1,12 @@
-function gridTraveler(m, n) {
+function gridTraveler(m, n, memo = {}) {
+    const key = `${m},${n}`
+
+    if (memo[key]) return memo[key]
     if (m == 1 && n == 1) return 1
     if (m == 0 || n == 0) return 0
 
-    return gridTraveler(m - 1, n) + gridTraveler(m, n - 1)
+    memo[key] = gridTraveler(m - 1, n, memo) + gridTraveler(m, n - 1, memo)
+    return memo[key]
 }
 
 console.log(gridTraveler(18, 18))
