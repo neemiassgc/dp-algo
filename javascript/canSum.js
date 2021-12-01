1f9d9163fef3b768b72e0ac5b01a916d29f59916
Neemias Santos <neemiassgc@outlook.com>
2021-12-01 12:21:21 -0300

    Optimize to memoized solution of the CanSum problem in Javascript

--- javascript/canSum.js
+++ javascript/canSum.js
@@ -1,11 +1,14 @@
-const canSum = (target, numbers) => {
+const canSum = (target, numbers, memo = {}) => {
+    if (target in memo) return memo[target]
     if (target < 0) return false;
     if (target === 0) return true;
 
-    for (let i of numbers)
-        if (canSum(target - i, numbers)) return true
+    for (let i of numbers) {
+        memo[target] = canSum(target - i, numbers, memo)
+        if (memo[target]) return true
+    }
 
-    return false
+    return memo[target] = false
 }
 
-console.log(canSum(3000, [7, 14]))
+console.log(canSum(3553, [7, 14, 28]))
