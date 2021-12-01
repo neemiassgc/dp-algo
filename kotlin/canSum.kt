736b3d9b20d4f848b3723e7e79a003e2cc2772cb
Neemias Santos <neemiassgc@outlook.com>
2021-12-01 12:59:00 -0300

    Optimize to memoized solution of the CanSum problem in Kotlin

--- kotlin/canSum.kt
+++ kotlin/canSum.kt
@@ -1,11 +1,14 @@
-fun canSum(targetSum: Int, numbers: Array<Int>): Boolean {
+fun canSum(targetSum: Int, numbers: Array<Int>, memo: HashMap<Int, Boolean> = HashMap()): Boolean {
+    if (memo.containsKey(targetSum)) return memo[targetSum] ?: false
     if (targetSum < 0) return false;
     if (targetSum == 0) return true;
 
     numbers.forEach {
-        if (canSum(targetSum - it, numbers)) return true
+        memo[targetSum] = canSum(targetSum - it, numbers, memo)
+        if (memo[targetSum] ?: false) return true
     }
 
+    memo[targetSum] = false
     return false
 }
 
