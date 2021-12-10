f2d26fc4f58c944ec3f6b5c3ab9f730f21757c99
Neemias Santos <neemiassgc@outlook.com>
2021-12-10 19:11:17 -0300

    Optimize to memoized solution of the CanConstruct problem in Kotlin

--- kotlin/canConstruct.kt
+++ kotlin/canConstruct.kt
@@ -1,16 +1,25 @@
-fun canConstruct(target: String, wordBank: Array<String>): Boolean {
+fun canConstruct(target: String, wordBank: Array<String>, memo: MutableList<String> = mutableListOf()): Boolean {
     if (target.isEmpty()) return true;
+    if (target in memo) return false
 
     wordBank.forEach { word ->
         if (target.startsWith(word)) {
             val suffix: String = target.slice(word.length..(target.length - 1))
-            if (canConstruct(suffix, wordBank)) return true
+            val result: Boolean = canConstruct(suffix, wordBank, memo)
+            
+            if (result) return true
+
+            memo += target
         }
     }
 
+
+    memo += target
     return false
 }
 
 fun main(): Unit {
     println(canConstruct("skateboard", arrayOf("bo", "rd", "ate", "t", "ska", "sk", "boar")))
+    println(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
+        arrayOf("e", "ee", "eee", "eeee", "eeeee", "eeeeee")))
 }
