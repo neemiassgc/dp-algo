a858287c6221735b635e60cd19d562ca776aa6e2
Neemias Santos <neemiassgc@outlook.com>
2021-12-12 15:09:31 -0300

    Optimize to memoized solution of the CountConstruct problem in Javascript

--- javascript/countConstruct.js
+++ javascript/countConstruct.js
@@ -1,13 +1,19 @@
-function countConstruct(target, wordBank) {
+function countConstruct(target, wordBank, memo = []) {
+    if (memo[target]) return memo[target];
     if (target.length === 0) return 1;
 
     let count = 0;
 
-    for (let word of wordBank)
-        if (target.startsWith(word))
-            count += countConstruct(target.substr(word.length), wordBank);
+    for (let word of wordBank) {
+        if (target.startsWith(word)) {
+            memo[target] = countConstruct(target.slice(word.length), wordBank, memo);
+            count += memo[target];
+        }
+    }
 
     return count;
 }
 
 console.log(countConstruct("purple", ["purp", "p", "ur", "le", "purpl"]))
+console.log(countConstruct("javascript", ["java", "scr", "it", "javascr", "script"]))
+console.log(countConstruct("aaaaaaaaaaaaaaaaaaa", ["a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa"]))
