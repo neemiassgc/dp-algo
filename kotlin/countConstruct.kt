b017e59f040595dcb21d53495d0b6e731bf45e02
Neemias Santos <neemiassgc@outlook.com>
2021-12-12 15:01:30 -0300

    Optimize to memoized solution of the CountConstruct problem in Kotlin

--- kotlin/countConstruct.kt
+++ kotlin/countConstruct.kt
@@ -1,16 +1,22 @@
-fun countConstruct(target: String, wordBank: Array<String>): Int {
+fun countConstruct(target: String, wordBank: Array<String>, memo: HashMap<String, Int> = hashMapOf()): Int {
+    if (memo.containsKey(target)) return memo[target]!!
     if (target.isEmpty()) return 1
 
     var count = 0;
 
     wordBank.forEach { word ->
-        if (target.startsWith(word))
-            count += countConstruct(target.substring(word.length), wordBank)
+        if (target.startsWith(word)) {
+            memo[word] = countConstruct(target.substring(word.length), wordBank)
+            count += memo[word]!!
+        }
     }
 
+    memo[target] = count
     return count
 }
 
 fun main(): Unit {
     println(countConstruct("purple", arrayOf("purp", "p", "ur", "le", "purpl")))
+    println(countConstruct("eeeeeeeeeeeeeeeeeeeeeeeeef",
+        arrayOf("e", "ee", "eeee", "eeeee", "eeeeee", "eeeeeee", "eeeeeeee", "eeeeeeeee", "eeeeeeeeee")))
 }
