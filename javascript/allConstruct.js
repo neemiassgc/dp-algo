80430e3bfdd6c970f2d54cfc762f4ea8634bac13
Neemias Santos <neemiassgc@outlook.com>
2021-12-16 12:23:10 -0300

    Optimize to memoized solution of the AllConstruct problem in Javascript

--- javascript/allConstruct.js
+++ javascript/allConstruct.js
@@ -1,20 +1,22 @@
-function allConstruct(target, wordBank) {
+function allConstruct(target, wordBank, memo = []) {
+    if (target in memo) return memo[target];
     if (target.length === 0) return [[]]
 
     let allWays = []
 
     for (let word of wordBank) {
         if (target.startsWith(word)) {
-            const suffixWays = allConstruct(target.substring(word.length), wordBank)
-            const targetWays = suffixWays.map(way => [word, ...way])
+            const rest = allConstruct(target.substring(word.length), wordBank, memo)
+            const targetWays = rest.map(way => [word, ...way])
             allWays.push(...targetWays)
         }
     }
 
+    memo[target] = allWays
     return allWays
 }
 
 console.log(allConstruct("purple", ["purp", "p", "ur", "le", "purpl"]))
 console.log(allConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd", "ef", "c"]))
 console.log(allConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar", "skate"]))
-console.log(allConstruct("aaaaaaaaaaaaaaaaaaaaaaaaaz", ["a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa"]))
+console.log(allConstruct("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaz", ["a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa"]))
