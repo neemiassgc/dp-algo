f0a1e0b7eebe127153cca5392b3ba609a1edae10
Neemias Santos <neemiassgc@outlook.com>
2021-12-16 12:26:55 -0300

    Optimize to memoized solution of the AllConstruct problem in Kotlin

--- kotlin/allConstruct.kt
+++ kotlin/allConstruct.kt
@@ -1,16 +1,18 @@
-fun allConstruct(target: String, wordBank: Array<String>): List<List<String>> {
+fun allConstruct(target: String, wordBank: Array<String>, memo: HashMap<String, List<List<String>>> = hashMapOf()): List<List<String>> {
+    if (memo.containsKey(target)) return memo[target]!!
     if (target.isEmpty()) return listOf(listOf())
 
     val allWays: MutableList<List<String>> = mutableListOf()
 
     wordBank.forEach { word ->
         if (target.startsWith(word)) {
-            val remainingWays: List<List<String>> = allConstruct(target.slice(word.length..(target.length - 1)), wordBank);
-            val targetWays: List<List<String>> = remainingWays.map() { way -> listOf(word) + way }
+            val rest: List<List<String>> = allConstruct(target.slice(word.length..(target.length - 1)), wordBank, memo);
+            val targetWays: List<List<String>> = rest.map() { way -> listOf(word) + way }
             targetWays.forEach(allWays::add)
         }
     }
 
+    memo[target] = allWays
     return allWays
 }
 
@@ -19,4 +21,5 @@ fun main(): Unit {
     println(allConstruct("strawberry", arrayOf("straw", "st", "ra", "w", "ber", "berry")))
     println(allConstruct("yellow", arrayOf("y", "ll", "o", "ow", "w", "e", "llow", "yel", "low", "ellow")))
     println(allConstruct("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaf", arrayOf("a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa")))
+    println(allConstruct("", arrayOf("ab", "ba")))
 }
